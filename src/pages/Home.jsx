706266e5b47bcd96226d7daa437265ed7f548249
Narzis111 <narzisnahar111@gmail.com>
2024-05-12 18:52:34 +0100

    FAQ section done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../components/Banner/Banner";
+import FAQs from "../components/FAQs/FAQs";
+import Feature from "../components/Feature/Feature";
+
+const Home = () => {
+    return (
+        <div>
+          <Banner></Banner>  
+          <Feature></Feature>
+          <FAQs></FAQs>
+        </div>
+    );
+};
+
+export default Home;
